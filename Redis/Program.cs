bb9a08d04c574b5d5089214b868223f2e477ce28
Yalçın ORUÇ <Yaln ORU@DESKTOP-7HODA89>
2023-07-05 02:04:55 +0300

    Add project files.

---
+++ Redis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Redis
+{
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string redisConnectionString = "redis-14441.c11.us-east-1-3.ec2.cloud.redislabs.com:14441";
+            Redis redisExample = new Redis(redisConnectionString);
+
+            // Veri ekleme
+            redisExample.VeriEkle("key1", "value1");
+            redisExample.VeriEkle("key2", "value2");
+
+            // Veri getirme
+            string value1 = redisExample.VeriGetir("key1");
+            string value2 = redisExample.VeriGetir("key2");
+            string value3 = redisExample.VeriGetir("key3");
+
+            // Veri silme
+            redisExample.VeriSil("key1");
+            redisExample.VeriSil("key2");
+            redisExample.VeriSil("key3");
+        }
+    }
+
+}
---
+++ Redis/RedisClass.cs
@@ -0,0 +1,61 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    public class Redis
+    {
+
+        private readonly ConnectionMultiplexer _redis;
+
+        public bool IsNullOrEmpty { get; private set; }
+
+        public Redis(string redisConnectionString)
+        {
+            _redis = ConnectionMultiplexer.Connect(redisConnectionString);
+        }
+
+        public void VeriEkle(string key, string value)
+        {
+            IDatabase db = _redis.GetDatabase();
+            db.StringSet(key, value);
+            Console.WriteLine("Veri eklendi: Key = {0}, Value = {1}", key, value);
+        }
+
+        public void VeriSil(string key)
+        {
+            IDatabase db = _redis.GetDatabase();
+            bool silindiMi = db.KeyDelete(key);
+
+            if (silindiMi)
+            {
+                Console.WriteLine("Veri silindi: Key = {0}", key);
+            }
+            else
+            {
+                Console.WriteLine("Veri bulunamadı: Key = {0}", key);
+            }
+        }
+
+        public string VeriGetir(string key)
+        {
+            IDatabase db = _redis.GetDatabase();
+            string value = db.StringGet(key);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("Veri getirildi: Key = {0}, Value = {1}", key, value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Veri bulunamadı: Key = {0}", key);
+                return null;
+            }
+        }
+    }
+}
